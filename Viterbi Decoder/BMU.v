b9fea1c02d0ea449e6367c6d482a86e2152a3701
Anirudh Ramkumar <110461428+VKACoder@users.noreply.github.com>
2025-01-02 20:18:13 +0530

    Update BMU.v

--- Viterbi Decoder/BMU.v
+++ Viterbi Decoder/BMU.v
@@ -2,30 +2,35 @@ module BMU(
     input [1:0] istate,
     input [1:0] cstate,
     input [1:0] rbits,
-    output reg [1:0] bm
+    input start,
+    output reg [1:0] bm,
+    output reg done
     );
     reg [1:0] abits; //Actual encoded bits
     reg [1:0] exor_value;
     
-    always @ (*)
+    always @ (start)
     begin
-        case ({cstate, istate})
-            4'b 0000: abits = 2'b 00;
-            4'b 0001: abits = 2'b 10;
-            4'b 0110: abits = 2'b 11;
-            4'b 0111: abits = 2'b 01;
-            4'b 1000: abits = 2'b 11;
-            4'b 1001: abits = 2'b 01;
-            4'b 1110: abits = 2'b 00;
-            4'b 1111: abits = 2'b 10;
-            default: abits = 2'b 00;
-        endcase
+        if (start == 1'b 1)
+        begin
+            case ({cstate, istate})
+                4'b 0000: abits = 2'b 00;
+                4'b 0001: abits = 2'b 10;
+                4'b 0110: abits = 2'b 11;
+                4'b 0111: abits = 2'b 01;
+                4'b 1000: abits = 2'b 11;
+                4'b 1001: abits = 2'b 01;
+                4'b 1110: abits = 2'b 00;
+                4'b 1111: abits = 2'b 10;
+                default: abits = 2'b 00;
+            endcase
+        end
     end
     
     //Calculate hamming distance - No. of bits that are same in rbits and abits
     always @ (abits)
     begin
-        exor_value = abits ^ rbits;
+        exor_value = {abits[1] ^ rbits[1], abits[0] ^ rbits[0]};
     end
     
     always @ (exor_value)
@@ -35,5 +40,6 @@ module BMU(
             2'b 01, 2'b 10: bm = 1;
             2'b 11: bm = 2;
         endcase
+        done = 1'b 1;
     end
 endmodule
