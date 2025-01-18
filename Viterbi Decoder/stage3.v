fd1233d0f98d4f00462c28a19a5f1e096ccc5cee
Anirudh Ramkumar <110461428+VKACoder@users.noreply.github.com>
2025-01-19 00:27:47 +0530

    Create stage3.v

---
+++ Viterbi Decoder/stage3.v
@@ -0,0 +1,18 @@
+module stage3(
+    input [11:0] din,
+    input clk,
+    input rst,
+    input start,
+    output [3:0] bm0,
+    output [3:0] bm1,
+    output [3:0] bm2,
+    output [3:0] bm3,
+    output [11:0] p0,
+    output [11:0] p1,
+    output [11:0] p2,
+    output [11:0] p3,
+    output done
+    );
+    
+    
+endmodule
