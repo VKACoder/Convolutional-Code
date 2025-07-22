b0e0d9421dce6e603b88310ecb4dd94157b2f9f4
Anirudh Ramkumar <110461428+VKACoder@users.noreply.github.com>
2025-07-22 18:04:03 +0530

    Update encoder_ShiftReg.v

--- Encoder/encoder_ShiftReg.v
+++ Encoder/encoder_ShiftReg.v
@@ -11,8 +11,8 @@ module encoder (input Din, input clk, input rst, output reg [1:0] Dout);
         end
       else
         begin
-          Dout[1] <= Din ^ shift_reg[1];
-          Dout[0] <= Din ^ shift_reg[1] ^ shift_reg[0];
+          Dout[0] <= Din ^ shift_reg[1];
+          Dout[1] <= Din ^ shift_reg[1] ^ shift_reg[0];
           shift_reg <= {Din, shift_reg[1]};          
         end
     end
